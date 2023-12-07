13c41ec3710865f5396a5abddc2280e83cbec742
Anderson Felipe Feitoza santos <andersonsd97@gmail.com>
2023-12-06 21:30:13 -0300

    transferência de pasta exemplos rapidos

---
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -0,0 +1,7 @@
+//repetir a construção do código = 1/5
+//construir o código = @    
+//depuração de erros = @
+//verificar todas as etapas com o console.log = @    
+//passar a ideia do código para o papel = @     
+//ler a explicação do livro = 
+
