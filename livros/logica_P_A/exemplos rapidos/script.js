7304adccfc033ac1fc3fecf4ef9f8cc16de59f16
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2023-12-11 08:32:11 -0300

    Modificação de arquivos na pasta livros, pesquisa e estudo sobre estrutura de repetição, break e continue

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -1,32 +1,12 @@
-//repetir a construção do código = 5/5
+//repetir a construção do código = 2/5
 //construir o código = @    
 //depuração de erros = @
 //verificar todas as etapas com o console.log = @    
 //passar a ideia do código para o papel = @     
 //ler a explicação do livro = @
 
-var inNumero = document.getElementById("inNumero");
-var btExibir = document.getElementById("btExibir");
-var outResposta = document.getElementById("outResposta");
+var list = ["BMW", "Volvo", "Saab", "Ford"];
 
-function exibirEstrelas() {
-    var numero = Number(inNumero.value);
-    
-    if(numero == 0 || numero == "" || isNaN(numero)) {
-        alert("Informe um número natural válido!");
-        inNumero.value = "";
-        inNumero.focus();
-        return
-    };
-
-    var estrelas = "";
-
-    for(var index = 1; index <= numero; index++) {
-        for(var i = 1; i <= index; i++) {
-            estrelas += "*"
-        };
-        estrelas += "\n"
-    };
-    outResposta.textContent = estrelas
-};
-btExibir.addEventListener("click", exibirEstrelas)
+list: {
+    console.log(list[])
+}
