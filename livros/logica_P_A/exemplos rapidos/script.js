9dab68d45d8161f9e7c8f3eb9086873f159b279c
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2023-12-12 06:51:40 -0300

    modificação da pasta livros, assuntos break e continue

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -1,12 +1,24 @@
-//repetir a construção do código = 2/5
+//repetir a construção do código = 1/5
 //construir o código = @    
 //depuração de erros = @
 //verificar todas as etapas com o console.log = @    
 //passar a ideia do código para o papel = @     
 //ler a explicação do livro = @
 
-var list = ["BMW", "Volvo", "Saab", "Ford"];
+function teste(valor) {
+    let cont = 0;
 
-list: {
-    console.log(list[])
+    while(valor < 50000) {
+        //valor = valor + 2
+        valor += 2
+        //cont = cont + 1
+        cont++
+
+        if(cont > 200) {
+            console.log("Não foi possível processar todos os dados");
+            break
+        }
+    }
+    console.log("cont: " + cont)
+    console.log("valor: " + valor)
 }
