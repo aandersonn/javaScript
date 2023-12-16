f6f2d9e7a2265499e2647dc4189be9ecfdf61707
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2023-12-16 13:56:30 -0300

    Modificações mmatinais na pasta livro

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -5,12 +5,34 @@
 //passar a ideia do código para o papel = @     
 //ler a explicação do livro = @
 
-for(var i = 10; i > 0; i--) {
-    console.log(i)
-    if(i === 5) {
-        break
+var inDescricao = document.getElementById("inDescricao");
+var inValor = document.getElementById("inValor");
+var btExibir = document.getElementById("btExibir");
+var outResposta = document.getElementById("outResposta");
+var outTotal = document.getElementById("outTotal");
+
+var numContas = 0;
+var valorTotal = 0;
+var resposta = ""
+
+function exibirConta() {
+    var descricao = inDescricao.value;
+    var valor = Number(inValor.value);
+
+    if(descricao == "") {
+        alert("Informe a descrição da conta.")
+        inDescricao.focus()
     }
 
+    if(valor == 0 || valor == "" || isNaN(valor)) {
+        alert("informe o valor da conta.");
+        inValor.value = "";
+        inValor.focus()
+    }
+
+    numContas++;
+    valorTotal = valorTotal + valor;
+
+    resposta = resposta + descricao + " - R$: " + valor
 }
 
-console.log("Deu o break")
