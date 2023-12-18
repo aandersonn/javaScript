25236bff6e32804aa9da01dd0ddd93356d7b83de
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2023-12-18 08:16:55 -0300

    Modificações matinais na pasta livros

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -5,40 +5,7 @@
 //passar a ideia do código para o papel = @     
 //ler a explicação do livro = @
 
-var inDescricao = document.getElementById("inDescricao");
-var inValor = document.getElementById("inValor");
-var btExibir = document.getElementById("btExibir");
-var outResposta = document.getElementById("outResposta");
-var outTotal = document.getElementById("outTotal");
-
-var numContas = 0;
-var valorTotal = 0;
-var resposta = ""
-
-function exibirConta() {
-    var descricao = inDescricao.value;
-    var valor = Number(inValor.value);
-
-
-    if(valor == 0 || isNaN(valor)) {
-        alert("informe o valor da conta.");
-        inValor.value = "";
-        inValor.focus()
-    }
-
-    numContas++;
-    valorTotal = valorTotal + valor;
-
-    resposta = resposta + descricao + " - R$: " + valor.toFixed(2) + "\n";
-
-    outResposta.textContent = resposta + "----------------------------------";
-
-    outTotal.textContent = numContas + " Conta(s) - total R$: " + valorTotal.toFixed(2);
-
-    inDescricao.value = "";
-    inValor.value = "";
-    inDescricao.focus()
+function a() {
+    var numero = 1;
+    b
 }
-
-btExibir.addEventListener("click", exibirConta)
-
