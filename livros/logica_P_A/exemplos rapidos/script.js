fab46741725a189011d5e06bbff99bf6e9ae14ee
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2023-12-21 08:24:34 -0300

    Modificação matinal na pasta livros/logica_p_A

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -5,10 +5,22 @@
 //passar a ideia do código para o papel = @     
 //ler a explicação do livro = @
 
-const estrutura = {
-    pessoa: {
-        nome: 'Anderson'
+function hello(name) {
+    let welcome = `Bem vindo ${name}`;
+    write(welcome, 1);
+}
+
+function write(phrase, type) {
+    switch(type) {
+        case 1: 
+            console.log(phrase);
+            break
+        case 2:
+            alert(phrase);
+            break
+        default:
+            document.write(phrase)
     }
 }
 
-copy(estrutura)
+hello("hcode")
