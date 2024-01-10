edb8f539898c148c01d65c71951c816e723d2c6a
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-01-10 20:48:27 -0300

    modificações matinais na pasta livros

--- livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_1_consultorio_odontologico/script.js
+++ livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_1_consultorio_odontologico/script.js
@@ -1,10 +1,37 @@
+//Cria referência aos elementos de entrada e saída de dados da página
 var inPaciente = document.getElementById("inPaciente");
 var btAdicionar = document.getElementById("btAdicionar");
 var btUrgencia = document.getElementById("btUrgencia");
 var btAtender = document.getElementById("btAtender");
+var outAtendimento = document.getElementById("outAtendimento");
+var outLista = document.getElementById("outLista");
+
+//Declara vetor global
 var paciente = [];
 
 function adicionarPaciente() {
+    //obtém nome do paciente
     var nome = inPaciente.value;
-    if()
+
+    //Verifica preenchimento 
+    if(nome == "") {
+        alert("Informe o nome da paciente");
+        inPaciente.focus();
+        return
+    }
+
+    paciente.push(nome);
+
+    var lista = "";
+
+    for(i = 0; i < paciente.length; i++) {
+        lista += (i + 1) + "." + paciente[i] + "\n"
+    };
+
+    outLista.textContent = lista;
+
+    inPaciente.value = "";
+    inPaciente.focus()
 }
+
+btAdicionar.addEventListener("click", adicionarPaciente)
