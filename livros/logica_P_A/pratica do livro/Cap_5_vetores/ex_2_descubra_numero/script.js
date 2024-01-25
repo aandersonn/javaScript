e2f015a206e21d2a1f536644c9fc849831ce2604
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-01-25 07:34:24 -0300

    modificações matinais na pasta livros

--- livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_2_descubra_numero/script.js
+++ livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_2_descubra_numero/script.js
@@ -9,7 +9,7 @@ let erros = [];
 let numeroGerado = Math.floor(Math.random() * 100) + 1;
 const numeroDeChances = 6;
 
-function apostarNumero {
+function apostarNumero() {
     let numeroApostado = Number(inNumero.value);
 
     //Checa a validade do número
@@ -30,7 +30,11 @@ function apostarNumero {
         //caso não acerte, terá duas opções: O número de chances acabar ou apostar em um número repetido
     } else {
         //Verifica se o número é repetido
-    } {
+        if(erros.indexOf(numeroApostado) >= 0) {
+            alert(`Você já apostou o número ${numeroApostado}. tente outro número...`  );
+
+            //atualiza as chances até acabar as chances do jogador
+        } else {
         erros.push(numeroApostado);
         let numeroDeErros = erros.length;
         let chancesRestantes = numeroDeChances - numeroDeErros;
@@ -49,9 +53,19 @@ function apostarNumero {
             let dica = numeroApostado < numeroGerado ? 'Maior': 'Menor';
             outDica.textContent = `Dica: tente um número ${dica} que o número ${numeroApostado}`;
         }
-    }   
+     } 
+    }  
+    inNumero.value = "";
+    inNumero.focus(); 
 }
 
-inNumero.value = "";
-inNumero.focus();
+btnApostar.addEventListener('click', apostarNumero);
+
+function reiniciarJogo() {
+    location.reload();
+}
+
+btnJogarNovamente.addEventListener('click', reiniciarJogo)
+
+
 
