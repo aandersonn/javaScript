a1781ca9f38ce64831daae0aa5b73867bd0c7391
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-02-02 07:40:36 -0300

    modificações matinais na pasta livros

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -1,15 +1,12 @@
-var idade = [12, 16, 15, 17, 14];
-var maiores = false;
-for(i = 0; i < idade.length; i++){
-    if(idade[i] >= 18) {
-        alert(idade[i]);
-        maiores = true
-    }
-}
+var nomes = ["Pedro", "Ana", "João"];
 
-if(!maiores) {
-    alert("Não há idades de maiores na lista")
-}
+nomes.sort();
+
+alert(nomes.join(", "));
+
+nomes.reverse();
+
+alert(nomes.join(", "))
 
 
 
--- livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_3_revenda_herbie/script.js
+++ livros/logica_P_A/pratica do livro/Cap_5_vetores/ex_3_revenda_herbie/script.js
@@ -42,6 +42,34 @@ btnAdicionar.addEventListener("click", adicionarCarrro)
         
         for(let index = 0; index < carros.length; index++) {
             //adiciona os carros à listagem
-            lista += `${carros[index].modelo}`
+            lista += `${carros[index].modelo} - R$: ${(carros[index].preco).tofixed(2)}\n`
+        }
+        outLista.textContent = lista
+    }
+
+    btnListar.addEventListener("click", listarCarros)
+
+    function filtrarCarros() {
+        let valorMaximo = Number(prompt('Qual a faixa de preço do carro que procura!'));
+
+        if(valorMaximo == 0 || isNaN(valorMaximo)) {
+            return
+        }
+
+        let lista = '';
+
+        for(let index = 0; index < carros.length; index++) {
+
+            if(carros[index].preco <= valorMaximo) {
+                lista += `${carros[index].modelo} - R$ ${(carros[index].preco).tofixed(2)}\n`
+            }
+
+            if(lista == '') {
+                outLista.textContent = `Não há carros na faixa de R$: ${(valorMaximo).toFixed(2)}`
+            } else {
+                outLista.textContent = `Carros com preços até R$: ${valorMaximo.toFixed(2)}\n ------------------------------ \n ${lista} \n`
+            }
         }
     }
+
+    btnFiltrar.addEventListener('click', filtrarCarros)
