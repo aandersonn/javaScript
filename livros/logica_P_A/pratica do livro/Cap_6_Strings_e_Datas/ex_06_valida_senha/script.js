4a25627ae6b2e64e5413768fd63b3722bd99f4c5
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-03-14 06:15:08 -0300

    modificações matinais na pasta livros

---
+++ livros/logica_P_A/pratica do livro/Cap_6_Strings_e_Datas/ex_06_valida_senha/script.js
@@ -0,0 +1,27 @@
+const inSenha = document.getElementById("inSenha");
+const btnSenha = document.getElementById("btnSenha");
+const outResposta = document.getElementById("outResposta");
+
+function verificarSenha() {
+    let senha = inSenha.value;
+    let erro = [];
+
+    if(senha.length < 8 || senha.length > 15) {
+        erro.push("Possuir entre 8 e 15 caracteres");
+
+    }
+
+    if(senha.match(/[0-9]/g) == null) {
+        erro.push("Possuir números (no mínimo, 1)");
+    }
+
+    if(!senha.match(/[a-z]/g)) {
+        erro.push("Possuir letras minúsculas (no mínimo, 1)");
+    }
+
+    if(!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g).length == 1) {
+        erro.push("Possuir letras maiúsculas (no mínimo, 2)");
+    }
+
+    
+}
