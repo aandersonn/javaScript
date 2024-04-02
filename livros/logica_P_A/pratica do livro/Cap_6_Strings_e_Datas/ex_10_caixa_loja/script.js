c566be798fbb69c7a4b9fcf15228ea4e4aca780e
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-04-02 08:26:58 -0300

    modificações matinais na pasta livros

---
+++ livros/logica_P_A/pratica do livro/Cap_6_Strings_e_Datas/ex_10_caixa_loja/script.js
@@ -0,0 +1,8 @@
+let inDataVencimento = document.getElementById("inDataVencimento");
+let inValor = document.getElementById("inValor");
+let inMulta = document.getElementById("inMulta");
+let inJuros = document.getElementById("inJuros");
+let inTotal = document.getElementById("inTotal");
+let btnNovaConta = document.getElementById("btnNovaConta");
+
+const taxa_multa = 2 / 100;
