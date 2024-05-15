7d50897722db77c78b37f27a38fe0ff68207f5d5
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-05-15 05:28:15 -0300

    alterações na pasta livros

--- livros/logica_P_A/pratica do livro/Cap_7_Funcoes_e_Eventos/ex004_calculo_parcelas/script.js
+++ livros/logica_P_A/pratica do livro/Cap_7_Funcoes_e_Eventos/ex004_calculo_parcelas/script.js
@@ -1,1 +1,15 @@
 const inPreco = document.getElementById("inPreco");
+const btExibir = document.getElementById("btExibir");
+const outParcelas = document.getElementById("outParcelas");
+
+btExibir.addEventListener("click", function () {
+    let preco = Number(inPreco.value);
+
+    let lista = "";
+
+    for (let i = 1; i <= 10; i++) {
+
+        lista += i + "x de R$: " + (preco / i).toFixed(2) + "\n";
+    }
+    outParcelas.textContent = "Opções de pagamento\n\n" + lista
+})
