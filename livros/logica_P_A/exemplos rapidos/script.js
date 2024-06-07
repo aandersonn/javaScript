450c2606a98f05cb885a17e90bf93649ed1158fd
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-06-07 09:37:25 -0300

    alterações na pasta livros

--- livros/logica_P_A/exemplos rapidos/script.js
+++ livros/logica_P_A/exemplos rapidos/script.js
@@ -1,9 +1,2 @@
-var app = "App de controle financeiro";
-console.log(app);
-var app2 = app.replace(" ", "");
-console.log(app2);
-var app3 = app.replace(/ /g, "");
-console.log(app3);
-var app4 = app.replace(/ /g, "").toLowerCase();
-console.log(app4)
+
 
