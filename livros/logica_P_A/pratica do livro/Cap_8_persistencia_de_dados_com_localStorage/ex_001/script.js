30c94d4e0988910695895271873429eb3650b93b
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-07-11 22:30:09 -0300

    alterações na pasta livros

--- livros/logica_P_A/pratica do livro/Cap_8_persistencia_de_dados_com_localStorage/ex_001/script.js
+++ livros/logica_P_A/pratica do livro/Cap_8_persistencia_de_dados_com_localStorage/ex_001/script.js
@@ -12,5 +12,36 @@ function trocarClube() {
         clube = "Farroupilha"
     }
 
-    divTitulo.className = "row-cores" + clube
+    divTitulo.className = "row-cores" + clube;
+
+    imgClub.src = "img/" + clube.toLowerCase() + ".png";
+    imgClub.className = "exibe";
+    imgClub.alt = "Símbolo do " + clube;
+
+    localStorage.setItem("clube", clube);
 }
+
+var rbBrasil = document.getElementById("rbBrasil");
+var rbPelotas = document.getElementById("rbPelotas");
+var rbFarroupilha = document.getElementById("rbFarroupilha");
+
+rbBrasil.addEventListener("change", trocarClube);
+rbPelotas.addEventListener("change", trocarClube);
+rbFarroupilha.addEventListener("change", trocarClube)
+
+function verificarClube() {
+    if(localStorage.getItem("clube")) {
+        var clube = localStorage.getItem("clube");
+
+        if(clube == "Brasil") {
+            rbBrasil.checked = true
+        } else if (clube == "Pelotas") {
+            rbPelotas.checked = true
+        } else {
+            rbFarroupilha.checked = true
+        }
+        trocarClube()
+    }
+}
+
+verificarClube()
