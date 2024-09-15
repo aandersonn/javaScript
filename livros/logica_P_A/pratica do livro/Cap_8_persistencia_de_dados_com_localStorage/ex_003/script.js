8fe7e8b70340dd1a4ddaaa7a546b80bb76065527
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-09-15 13:34:02 -0300

    alterações na pasta java script

--- livros/logica_P_A/pratica do livro/Cap_8_persistencia_de_dados_com_localStorage/ex_003/script.js
+++ livros/logica_P_A/pratica do livro/Cap_8_persistencia_de_dados_com_localStorage/ex_003/script.js
@@ -28,7 +28,7 @@ function incluirAposta() {
         localStorage.setItem("melanciaPeso", peso);
     }
 
-    mostrarAposta();
+    mostrarApostas();
     inNome.value = "";
     inPeso.value = "";
     inNome.focus();
@@ -71,7 +71,50 @@ function mostrarApostas() {
 mostrarApostas();
 
 function verificarVencedor() {
-    if(!localStorage.getItem("")) {
+    if(!localStorage.getItem("melanciaNome")) {
+        alert("Não há apostas cadastradas");
+        return;
+    }
 
+    var pesoCorreto = Number(prompt("Qual o peso correto da melancia?"))
+
+    if(pesoCorreto == 0 || isNaN(pesoCorreto)) {
+        return;
+    }
+
+    var nomes = localStorage.getItem("melanciaNome").split(";");
+    var pesos = localStorage.getItem("melanciaPeso").split(";");
+
+    var vencedorNome = nomes[0];
+    var vencedorPeso = Number(pesos[0]);
+
+    for (var i = 1; i < nomes.length; i++) {
+        difVencedor = Math.abs(vencedorPeso - pesoCorreto);
+        difAposta = Math.abs(Number(pesos[i]) - pesoCorreto)
+
+        if(difAposta < difVencedor) {
+            vencedorNome = nomes[i];
+            vencedorPeso = Number(pesos[i])
+        }
+    }
+
+    var mensagem = "Resultado - peso correto: " + pesoCorreto + "gr";
+    mensagem += "\n------------------------------------------------";
+    mensagem += "\nVencedor: " + vencedorNome;
+    mensagem += "\Aposta: " + vencedorPeso + "gr";
+    alert(mensagem)
+}
+
+var btVencedor = document.getElementById("btVencedor");
+btVencedor.addEventListener("click", verificarVencedor)
+
+function limparApostas() {
+    if(confirm("Confirma a exclusão de todas as apostas?")) {
+        localStorage.removeItem("melanciaNome");
+        localStorage.removeItem("melanciaPeso")
+        mostrarApostas()
     }
 }
+
+var btLimpar = document.getElementById("btLimpar");
+btLimpar.addEventListener("click", limparApostas)
