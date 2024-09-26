bd057242190564234c9a54d15bf47a2c09cf3f34
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-09-25 21:48:12 -0300

    aterações na pasta java script

--- livros/logica_P_A/pratica do livro/Cap_9_inserir_elementos_html_via_javascript/ex_01/script.js
+++ livros/logica_P_A/pratica do livro/Cap_9_inserir_elementos_html_via_javascript/ex_01/script.js
@@ -10,7 +10,44 @@ function adicionarTarefa() {
 
     var divQuadro = document.getElementById("divQuadro")
     var h5 = document.createElement("h5")
-    console.log(divQuadro)
+    var texto = document.createTextNode(tarefa)
+    h5.appendChild(texto)
+    divQuadro.appendChild(h5)
+
+    inTarefa.value = "";
+    inTarefa.focus()
+}
+var btAdicionar = document.getElementById("btAdicionar")
+btAdicionar.addEventListener("click", adicionarTarefa)
+
+function selecionarTarefa() {
+    var h5 = document.getElementsByTagName("h5")
+    var numH5 = h5.length;
+
+    if(numH5 == 0) {
+        alert("Não há tarefas para selecionar");
+        return
+    }
+
+    var aux = -1
+
+    for(var i = 0; i < numH5; i++) {
+        if(h5[i].className == "tarefaSelecionada") {
+            h5[i].className = "tarefaNormal";
+            aux = i;
+            break;
+        }
+    }
+
+    if(aux == numH5 - 1) {
+        aux = -1
+    }
+
+    h5[aux + 1].className = "tarefaSelecionada"
+
+    
 }
 
-adicionarTarefa()
+var btSelecionar = document.getElementById("btSelecionar");
+btSelecionar.addEventListener("click", selecionarTarefa)
+
