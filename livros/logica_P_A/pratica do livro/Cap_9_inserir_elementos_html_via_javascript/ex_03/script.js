c1d4d7ca6916f8bf49744de79150646e6c53159c
Anderson Felipe Feitoza santos <112564562+Fflipee@users.noreply.github.com>
2024-11-25 08:36:06 -0300

    alterações na pasta javascript

--- livros/logica_P_A/pratica do livro/Cap_9_inserir_elementos_html_via_javascript/ex_03/script.js
+++ livros/logica_P_A/pratica do livro/Cap_9_inserir_elementos_html_via_javascript/ex_03/script.js
@@ -1,4 +1,37 @@
 function adicionarFilme() {
     //Criar referência aos campos de entrada
-    var inTitulo = document.getElementById("inTitulo")
+    var inTitulo = document.getElementById("inTitulo");
+    var inGenero = document.getElementById("inGenero");
+    var titulo = inTitulo.value; //Obtém conteúdo dos campos
+    var genero = inGenero.value;
+
+    //Valida preenchimento
+    if(titulo == "" || genero == "") {
+        alert("Informe corretamente os dados");
+        inTitulo.focus();
+        return
+    }
+
+    //Cria referência ao elemento tbFilme
+    var tbFilme = document.getElementById("tbFilme");
+    
+    //Chama function que irá inserir filme na tabela
+    inserirLinha(tbFilme, titulo, genero);
+
+    //Chama function que irá gravar dados em localStorage
+    gravarFilme(titulo, genero)
+
+    inTitulo.value = "";//Limpa o campo de entrada
+    inGenero.value = "";
+    inTitulo.focus()//posiciona o cursor em inTitulo
+
+    console.log(inTitulo, inGenero, titulo, genero, tbFilme)
+}
+var btAdicionar = document.getElementById("btAdicionar");
+btAdicionar.addEventListener("click", adicionarFilme)
+
+adicionarFilme()
+
+function inserirLinha(tabela, titulo, genero) {
+
 }
